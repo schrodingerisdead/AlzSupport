fdfadabc03db96b55520c33bc31ed1721d8ced53
Magdalena Oreshkova <oreskovam7@gmail.com>
2024-02-17 07:40:03 +0100

    Initial Commit

---
+++ AlzSupport/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlzSupport
+//
+//  Created by Magdalena Oreshkova on 2/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
